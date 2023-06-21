aa22a23954b244ec21a06d238ff30c733d29532a
Paco <paco@mastermind.ac>
2023-06-21 15:53:36 +0200

    Version 1.19.4

--- src/main/java/ac/mastermind/pacomod/Tab/DeportesTab.java
+++ src/main/java/ac/mastermind/pacomod/Tab/DeportesTab.java
@@ -13,6 +13,8 @@ import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.CreativeModeTab;
+
 
 @Mod.EventBusSubscriber(modid = PacoMod.MODID,bus= Mod.EventBusSubscriber.Bus.MOD)
 public class DeportesTab {
@@ -21,28 +23,28 @@ public class DeportesTab {
     public static void buildContents(CreativeModeTabEvent.Register event) {
         event.registerCreativeModeTab(new ResourceLocation(PacoMod.MODID, "deportes_tab"),
                 builder ->
-                    // Set name of tab to display
-                    builder
-                        .title(Component.translatable("item_group." + PacoMod.MODID + ".deportes_tab"))
-                        // Set icon of creative tab
-                        .icon(() -> new ItemStack(OtrosItems.DEPORTE_TAB_ITEM.get()))
-                        // Add default items to tab
-                        .displayItems((params, output,displayShow) -> {
-                            for(RegistryObject<Item> item:DeportesItems.DEPORTES_ITEMS.getEntries()){
-                                output.accept(item.get());
-                            }
-                            for(RegistryObject<Item> item:DeportesItems.DEPORTES_ARMAS_ITEMS.getEntries()){
-                                output.accept(item.get());
-                            }
-                            for(RegistryObject<Item> item:DeportesItems.DEPORTES_HERRAMIENTAS_ITEMS.getEntries()){
-                                output.accept(item.get());
-                            }
-                            for(RegistryObject<Block> block:DeportesBlocks.DEPORTES_BLOCK.getEntries()){
-                                output.accept(block.get());
-                            }
-                            //output.accept(DeportesBlocks.GRADAS_BLOCK.get());
-                            output.accept(OtrosItems.LIQUIDO_ITEM.get());
-                        })
+                        // Set name of tab to display
+                        builder
+                                .title(Component.translatable("item_group." + PacoMod.MODID + ".deportes_tab"))
+                                // Set icon of creative tab
+                                .icon(() -> new ItemStack(OtrosItems.DEPORTE_TAB_ITEM.get()))
+                                // Add default items to tab
+                                .displayItems((params, output) -> {
+                                    for(RegistryObject<Item> item:DeportesItems.DEPORTES_ITEMS.getEntries()){
+                                        output.accept(item.get());
+                                    }
+                                    for(RegistryObject<Item> item:DeportesItems.DEPORTES_ARMAS_ITEMS.getEntries()){
+                                        output.accept(item.get());
+                                    }
+                                    for(RegistryObject<Item> item:DeportesItems.DEPORTES_HERRAMIENTAS_ITEMS.getEntries()){
+                                        output.accept(item.get());
+                                    }
+                                    for(RegistryObject<Block> block:DeportesBlocks.DEPORTES_BLOCK.getEntries()){
+                                        output.accept(block.get());
+                                    }
+                                    //output.accept(DeportesBlocks.GRADAS_BLOCK.get());
+                                    output.accept(OtrosItems.LIQUIDO_ITEM.get());
+                                })
         );
     }
 }
